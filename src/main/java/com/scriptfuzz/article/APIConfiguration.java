c0dc6ab5dd9b26fe48efc7ad5a5d87096e2ee109
jose Diaz <jrd.palacios@gmail.com>
2015-08-12 22:05:03 -0400

    Initial Commit

---
+++ src/main/java/com/scriptfuzz/article/APIConfiguration.java
@@ -0,0 +1,68 @@
+package com.scriptfuzz.article;
+
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+import freemarker.template.TemplateExceptionHandler;
+
+import java.io.StringWriter;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by J. Diaz on 08-12-15.
+ */
+public class APIConfiguration {
+
+    private static Configuration cfg;
+
+    public static void initializeFreemarker(){
+        // Configure freemarker template
+         cfg = new Configuration(Configuration.VERSION_2_3_23);
+        try {
+            cfg.setClassForTemplateLoading(ArticleAPI.class, "/");
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+        cfg.setDefaultEncoding("UTF-8");
+        cfg.setTemplateExceptionHandler(TemplateExceptionHandler.RETHROW_HANDLER);
+    }
+
+    /**
+     * Build an HTML represenation of all the routes supported in this API
+     * @return HTML representation of this API
+     * @throws Exception Error Parsing the freemarker template
+     */
+    public static String getHtmlApi() throws Exception{
+        Template apiTemplate = cfg.getTemplate("api.ftl");
+
+        Map<String, Object> root = new HashMap<>();
+
+        List<Map> seq = new ArrayList<>();
+        Map<String, String> map = new HashMap<>();
+
+        map.put("url", "/");
+        map.put("description", "Display API");
+
+        seq.add(map);
+
+        map = new HashMap<>();
+        map.put("url", "/article/all");
+        map.put("description", "Gets all articles");
+
+        seq.add(map);
+
+        map = new HashMap<>();
+        map.put("url", "/article/:year/:title");
+        map.put("description", "Gets article that matches year + title");
+
+        root.put("routes", seq);
+
+        StringWriter writer = new StringWriter();
+        apiTemplate.process(root, writer);
+
+        return writer.toString();
+    }
+}
