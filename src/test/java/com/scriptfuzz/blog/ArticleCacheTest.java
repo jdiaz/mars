8c4c3e39f9bd38240d8370cef8c3bb4e0cda7c43
josediaz30 <jose.diaz30@upr.edu>
2015-09-28 23:20:51 -0400

    Adding empty test cases

---
+++ src/test/java/com/scriptfuzz/blog/ArticleCacheTest.java
@@ -0,0 +1,24 @@
+package com.scriptfuzz.blog;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by josediaz on 9/28/15.
+ */
+public class ArticleCacheTest {
+
+    final String template = "";
+
+    @Before
+    public void populateCache(){
+      //  ArticleCache.addToCache();
+    }
+
+    @Test
+    public void addToCacheTest(){
+      //  ArticleCache.addToCache();
+
+      //  ArticleCache.getRecentArticles();
+    }
+}
---
+++ src/test/java/com/scriptfuzz/blog/ArticleDAOTest.java
@@ -0,0 +1,7 @@
+package com.scriptfuzz.blog;
+
+/**
+ * Created by josediaz on 9/28/15.
+ */
+public class ArticleDAOTest {
+}
